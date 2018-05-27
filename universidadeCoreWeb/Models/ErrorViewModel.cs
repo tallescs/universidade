0e61a2748b7fd4805f7a8cbda68f1488096bb106
tallescs <tallescs@gmail.com>
2018-05-27 13:46:04 -0300

    1º commit de código. Projeto padrão Asp.Net Core. Versão: 2.0.0. Template MVC Web Application

---
+++ universidadeCoreWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace universidadeCoreWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
